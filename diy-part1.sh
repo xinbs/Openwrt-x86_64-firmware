600c35450a795a01672fa3df9d8414022c175d7e
xinbs <42456443+xinbs@users.noreply.github.com>
2022-01-14 00:54:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
